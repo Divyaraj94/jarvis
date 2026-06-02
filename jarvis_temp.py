0373e02271d782a63b6361fe6bd48bd0ccce1de6
Divyaraj94 <divyarajsinhc92@gmail.com>
2026-06-03 03:55:35 +0530

    Untrack metadata/test files and update core code files

--- jarvis_temp.py
+++
@@ -1,9 +0,0 @@
-import urllib.request
-import json
-response = urllib.request.urlopen('https://hacker-news.firebaseio.com/v0/topstories.json')
-data = json.loads(response.read().decode())
-for i in range(3):
-    story_id = data[i]
-    response = urllib.request.urlopen(f'https://hacker-news.firebaseio.com/v0/item/{story_id}.json')
-    item_data = json.loads(response.read().decode())
-    print(f"{item_data['title']}")
